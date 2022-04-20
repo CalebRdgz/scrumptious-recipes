193e824d86fb70f33072e1ad2756e93a4d9bdebb
Caleb <calebrod1@yahoo.com>
2022-04-20 17:14:18 -0500

    Measure Model

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
+from recipes.models import Recipe
 
 
 # Register your models here.
+# ModelAdmin is already in django (it comes with django)
+class RecipeAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Recipe, RecipeAdmin)
--- recipes/models.py
+++ recipes/models.py
@@ -2,3 +2,29 @@ from django.db import models
 
 
 # Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=125)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.URLField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Measure(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    abbreviation = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+# class Ingredient(models.Model):
+
+#     amount =
+#     recipt =
+#     measure =
+#     food =
