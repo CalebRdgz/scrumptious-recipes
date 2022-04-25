989f33446a3924f1c415c77fa864d44855f44524
Caleb <calebrod1@yahoo.com>
2022-04-25 01:19:30 -0500

    Limit amount max value

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
 class Recipe(models.Model):
@@ -30,7 +30,7 @@ class FoodItem(models.Model):
 
 
 class Ingredient(models.Model):
-    amount = models.FloatField()
+    amount = models.FloatField(validators=[MaxValueValidator(20)])
     recipe = models.ForeignKey(
         "Recipe",
         related_name="ingredients",
